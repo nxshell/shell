cf842f0971fa37fe666ba5f02713013eefb5c988
沐雨 <baizw@cnns.net>
2022-10-08 11:33:48 +0800

    fix:  修复控制台路由报错

--- src/router/index.js
+++ src/router/index.js
@@ -3,6 +3,22 @@ import VueRouter from 'vue-router'
 import Home from '../views/Home.vue'
 
 Vue.use(VueRouter)
+const originalPush = VueRouter.prototype.push
+const originalReplace = VueRouter.prototype.replace
+
+VueRouter.prototype.push = function push(location, onResolve, onReject) {
+  if (onResolve || onReject) {
+    return originalPush.call(this, location, onResolve, onReject)
+  }
+  return originalPush.call(this, location).catch(err => err)
+}
+
+VueRouter.prototype.replace = function push(location, onResolve, onReject) {
+  if (onResolve || onReject) {
+    return originalReplace.call(this, location, onResolve, onReject)
+  }
+  return originalReplace.call(this, location).catch(err => err)
+}
 
 const routes = [
     {
