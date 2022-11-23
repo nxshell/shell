d56af7f077cf9b4cd12c0ef0d5524df745378ec6
剧终 <13824303245@163.com>
2022-11-23 11:38:45 +0800

    1. 修复终端右键菜单弹出位置异常

--- src/components/menu/menuManager.js
+++ src/components/menu/menuManager.js
@@ -41,7 +41,7 @@ const contextMenuDirective = {
             } else {
                 menu = binding.value;
             }
-            
+
             if (menu && typeof menu.then === "function") {
                 menu = await menu;
             }
@@ -71,7 +71,7 @@ function initDefaultMenuHandler() {
             closeAllMenu();
         }
         // setTimeout(() => {
-            
+
         // }, 100);
     }, true);
 
