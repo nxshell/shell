c02e8c9e441d787cb6cee7a303179bd7987b4749
剧终 <13824303245@163.com>
2022-12-05 17:16:21 +0800

    1. 修复编辑器主题颜色异常 2. 新增多款主题支持自行选择

--- src/views/editor/editorview.vue
+++ src/views/editor/editorview.vue
@@ -1,58 +1,56 @@
 <template xmlns="" xmlns="">
 	<div class="pt-vnc-view">
-		<pt-toolbar :centerStyle="{
-            overflow: 'hidden',
-            marginLeft: '10px',
-            marginRight: '10px'
-        }">
-			<template slot="left">
-				<i class="el-icon-folder-opened nx-edit-file-open" />
-			</template>
-			<div class="editor_url" slot="center">
-				{{ currentPath }}
+		<div class="n-editor-toolbar">
+			<div class="n-editor-toolbar__nav">
+				<div class="nav-wrapper">
+					<i class="el-icon-folder-opened nx-edit-file-open" />
+					<span class="editor_url">{{ currentPath }}</span>
+				</div>
 			</div>
-			<template slot="right">
+			<div class="n-editor-toolbar__actions">
+				<el-select v-model="editorTheme" placeholder="请选择主题" @change="themeChange">
+					<el-option v-for="(theme, index) in themes" :label="theme.name" :value="index" :key="index" />
+				</el-select>
 				<el-input
 					v-model="searchKeyWords"
 					:placeholder="T('home.editor.search-tab')"
 					class="nx-search-input"
 					suffix-icon="el-icon-search"
-					@keydown.enter.native="searchDown" />
-			</template>
-		</pt-toolbar>
+					@keydown.enter.native="searchDown"
+				/>
+			</div>
+		</div>
 		<div ref="editor" class="screen" :style="{ 'font-size': fontSize + 'px' }"></div>
 	</div>
 </template>
 
 <script>
-import { mapState } from "vuex";
+import { mapState } from 'vuex'
 
-import { EditorView, keymap, lineNumbers, highlightActiveLine } from "@codemirror/view"
-import { EditorSelection, SelectionRange, Compartment } from "@codemirror/state"
-import { defaultKeymap, history, historyKeymap } from "@codemirror/commands"
-import { highlightSelectionMatches, SearchCursor } from "@codemirror/search"
-import { syntaxHighlighting, defaultHighlightStyle } from "@codemirror/language"
+import { EditorView, keymap, lineNumbers, highlightActiveLine } from '@codemirror/view'
+import { EditorSelection, SelectionRange, Compartment } from '@codemirror/state'
+import { defaultKeymap, history, historyKeymap } from '@codemirror/commands'
+import { highlightSelectionMatches, SearchCursor } from '@codemirror/search'
 
-import { javascript } from "@codemirror/lang-javascript"
-import { css } from "@codemirror/lang-css"
-import { cpp } from "@codemirror/lang-cpp"
-import { html } from "@codemirror/lang-html"
-import { java } from "@codemirror/lang-java"
-import { json } from "@codemirror/lang-json"
-import { markdown } from "@codemirror/lang-markdown"
-import { php } from "@codemirror/lang-php"
-import { python } from "@codemirror/lang-python"
-import { xml } from "@codemirror/lang-xml"
-
-import { oneDarkTheme } from "@codemirror/theme-one-dark"
+import { javascript } from '@codemirror/lang-javascript'
+import { css } from '@codemirror/lang-css'
+import { cpp } from '@codemirror/lang-cpp'
+import { html } from '@codemirror/lang-html'
+import { java } from '@codemirror/lang-java'
+import { json } from '@codemirror/lang-json'
+import { markdown } from '@codemirror/lang-markdown'
+import { php } from '@codemirror/lang-php'
+import { python } from '@codemirror/lang-python'
+import { xml } from '@codemirror/lang-xml'
+import themes from './themes'
 
 export default {
-	name: "PtEditorView",
+	name: 'PtEditorView',
 	components: {},
 	props: {
 		mode: {
 			type: String,
-			default: "full"
+			default: 'full'
 		},
 		sessionId: {
 			type: Number
@@ -61,172 +59,182 @@ export default {
 
 	data() {
 		return {
-			editorValue: "",
-			oldVlaue: "",
-			currentPath: "",
-			searchKeyWords: "",
+			editorValue: '',
+			oldValue: '',
+			currentPath: '',
+			searchKeyWords: '',
 			fontSize: 16,
-			zoom: 0
-		};
+			zoom: 0,
+			editorTheme: 0,
+			editor_view: null,
+			themes,
+			themeConfig: new Compartment()
+		}
 	},
 
 	computed: {
-		...mapState(["theme"]),
+		...mapState(['theme'])
 	},
 	watch: {
 		searchKeyWords(newVal) {
-			if (newVal === "") {
-				this.clearSearch();
+			if (newVal === '') {
+				this.clearSearch()
 			} else {
-				this.doSearchInit();
+				this.doSearchInit()
 			}
+		},
+		theme(n, o) {
+			const theme = n === 'dark' ? 1 : n === 'light' ? 0 : 6
+			this.themeChange(theme)
 		}
 	},
 
 	created() {
-		this.init();
+		this.$nextTick(() => {
+			this.editorTheme = this.theme === 'dark' ? 1 : this.theme === 'light' ? 0 : 6
+			this.init()
+		})
 	},
 
 	methods: {
 		async init() {
 			if (this.sessionInstance) {
-				return;
+				return
 			}
 
-			let sessionInstance = this.$sessionManager.getSessionInstanceById(this.sessionId);
-			this.currentPath = sessionInstance?.remote_path ?? '';
+			let sessionInstance = this.$sessionManager.getSessionInstanceById(this.sessionId)
+			this.currentPath = sessionInstance?.remote_path ?? ''
 			sessionInstance.on('close', () => {
 				try {
-					this.$destroy();
-					this.$el.parentNode.removeChild(this.$el);
+					this.$el.parentNode.removeChild(this.$el)
+					this.$destroy()
 				} catch (e) {
-					console.log('sftp editor instance remove error', e);
+					console.log('sftp editor instance remove error', e)
 				}
-			});
-			await sessionInstance.init();
-			const editorValue = await sessionInstance.readFileContent();
-			this.editorValue = new TextDecoder().decode(editorValue);
+			})
+			await sessionInstance.init()
+			const editorValue = await sessionInstance.readFileContent()
+			this.editorValue = new TextDecoder().decode(editorValue)
 
 			sessionInstance.registerCloseCallback(() => {
-				this.save();
+				this.save()
 			})
 
-			this.sessionInstance = sessionInstance;
-			const lineWrapping = new Compartment();
-			const ctrl_s_key = [{
-				key: "Ctrl-s",
-				run: this.save
-			}, {
-				key: "Alt--",
-				run: this.zoom_in
-			}, {
-				key: "Alt-=",
-				run: this.zoom_out
-			}, {
-				key: "Alt-0",
-				run: this.zoom_over
-			}]
-			let extensions = [
+			this.sessionInstance = sessionInstance
+			const lineWrapping = new Compartment()
+			const ctrl_s_key = [
+				{
+					key: 'Ctrl-s',
+					run: this.save
+				},
+				{
+					key: 'Alt--',
+					run: this.zoom_in
+				},
+				{
+					key: 'Alt-=',
+					run: this.zoom_out
+				},
+				{
+					key: 'Alt-0',
+					run: this.zoom_over
+				}
+			]
+			const extensions = [
 				keymap.of([...defaultKeymap, ...historyKeymap, ...ctrl_s_key]),
 				lineNumbers(),
-				syntaxHighlighting(defaultHighlightStyle),
 				history(),
 				highlightActiveLine(),
 				highlightSelectionMatches(),
 				lineWrapping.of(EditorView.lineWrapping),
-				this.getSupportLangMode(sessionInstance.ext_name)
-			];
-			if (this.theme === "dark") {
-				extensions.push(oneDarkTheme)
-			}
-
+				this.getSupportLangMode(sessionInstance.ext_name),
+				this.themeConfig.of([themes[this.editorTheme]])
+			]
 			this.editor_view = new EditorView({
 				doc: this.editorValue,
 				extensions: extensions,
 				parent: this.$refs.editor
 			})
-			this.oldVlaue = this.editor_view.state.doc.toString();
+			this.oldValue = this.editor_view.state.doc.toString()
 		},
 		async save() {
-			this.editorValue = this.editor_view.state.doc.toString();
-			if (this.editorValue !== this.oldVlaue) {
-				await this.sessionInstance.writeFileContent(this.editorValue);
-				await this.sessionInstance.saveToRemote();
-				this.oldVlaue = this.editorValue;
+			this.editorValue = this.editor_view.state.doc.toString()
+			if (this.editorValue !== this.oldValue) {
+				await this.sessionInstance.writeFileContent(this.editorValue)
+				await this.sessionInstance.saveToRemote()
+				this.oldValue = this.editorValue
 			}
 		},
 		zoom_in() {
-			this.zoom -= 1;
-			this._zoom();
-			return true;
+			this.zoom -= 1
+			this._zoom()
+			return true
 		},
 		zoom_out() {
-			this.zoom += 1;
-			this._zoom();
-			return true;
+			this.zoom += 1
+			this._zoom()
+			return true
 		},
 		zoom_over() {
-			this.zoom = 0;
-			this._zoom();
-			return true;
+			this.zoom = 0
+			this._zoom()
+			return true
 		},
 		_zoom() {
-			const scale = Math.pow(1.1, this.zoom);
-			this.fontSize = 16 * scale;
+			const scale = Math.pow(1.1, this.zoom)
+			this.fontSize = 16 * scale
 		},
 		getSupportLangMode(type) {
 			switch (type) {
 				case '.js':
-					return javascript();
+					return javascript()
 				case '.css':
-					return css();
+					return css()
 				case '.cc':
-					return cpp();
+					return cpp()
 				case '.html':
 					return html()
 				case '.java':
-					return java();
+					return java()
 				case '.json':
-					return json();
+					return json()
 				case '.md':
-					return markdown();
+					return markdown()
 				case '.php':
-					return php();
+					return php()
 				case '.py':
-					return python();
+					return python()
 				case '.xml':
-					return xml();
+					return xml()
 				default:
 					return javascript()
-
 			}
 		},
 		clearSearch() {
-			this.searchKeyWords = "";
-			this.search_cursor = null;
+			this.searchKeyWords = ''
+			this.search_cursor = null
 		},
 		doSearchInit() {
-			this.search_cursor = new SearchCursor(this.editor_view.state.doc, this.searchKeyWords);
+			this.search_cursor = new SearchCursor(this.editor_view.state.doc, this.searchKeyWords)
 		},
 		searchDown() {
 			if (!this.search_cursor) {
-				return;
+				return
 			}
-			let curr = this.search_cursor.next();
+			let curr = this.search_cursor.next()
 			if (curr.done) {
-				return;
+				return
 			}
 			this.editor_view.dispatch({
-				selection: EditorSelection.create([
-					new SelectionRange(curr.value.from, curr.value.to)
-				]),
+				selection: EditorSelection.create([new SelectionRange(curr.value.from, curr.value.to)]),
 				scrollIntoView: true
 			})
+		},
+		themeChange(theme) {
+			this.editor_view.dispatch({
+				effects: this.themeConfig.reconfigure([themes[theme]])
+			})
 		}
-	},
-
-	async beforeDestroy() {
-
 	}
 }
 </script>
@@ -236,15 +244,44 @@ export default {
 	height: 100%;
 	width: 100%;
 	background-color: var(--n-bg-color-base);
-
-	::v-deep .el-input__inner {
-		border: 1px solid var(--n-bg-color-base) !important;
-	}
-
-	.nx-edit-file-open {
-		font-size: 18px;
-		color: var(--n-text-color-base);
-		transition: color .2s;
+	.n-editor-toolbar {
+		display: flex;
+		justify-content: flex-start;
+		align-items: center;
+		height: 40px;
+		padding: 0 5px;
+		&__nav {
+			flex: 1 0 0;
+			.nav-wrapper {
+				display: flex;
+				justify-content: flex-start;
+				align-items: center;
+				& > *:not(:last-child) {
+					margin-right: 5px;
+				}
+				.nx-edit-file-open {
+					font-size: 18px;
+					color: var(--n-text-color-base);
+					transition: color 0.2s;
+				}
+				.editor_url {
+					color: var(--n-text-color-base);
+					text-overflow: ellipsis;
+					white-space: nowrap;
+					overflow: hidden;
+					max-width: calc(100% - 20px);
+				}
+			}
+		}
+		&__actions {
+			flex: 1 0 0;
+			display: flex;
+			justify-content: flex-end;
+			align-items: center;
+			& > *:not(:first-child) {
+				margin-left: 5px;
+			}
+		}
 	}
 
 	.screen {
@@ -257,15 +294,5 @@ export default {
 			overflow: auto;
 		}
 	}
-
-	.editor_url {
-		color: var(--n-text-color-base);
-		text-overflow: ellipsis;
-		white-space: nowrap;
-		overflow: hidden;
-		max-width: 60%;
-	}
-
-	
 }
 </style>
