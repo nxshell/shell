e7ccb329b68ca1b034b057d49a261a21b67fa842
剧终 <13824303245@163.com>
2023-04-07 18:18:15 +0800

    1. 完成SSH会话界面编写及主题改造

--- src/views/session/components/xtermTheme/ThemePreview.vue
+++ src/views/session/components/xtermTheme/ThemePreview.vue
@@ -1,213 +1,244 @@
 <template>
-    <div class="n-terminal-preview" v-bind="$attrs">
-        <div class="color-dot-wrapper">
-            <span class="color-name">{{ props.themeName }}</span>
-            <div class="color-dot">
-                <span class="color-dot-item" v-for="(item,index) in colorList" :key="index" :style="{background: item}"></span>
-            </div>
-        </div>
-        <div class="theme-color" :style="themeStyle">
-            <div class="theme-color-item">
-                <span>[</span>
-                <span :style="{color: currentTheme.green}">root</span>
-                <span :style="{color: currentTheme.cyan}">@</span>
-                <span :style="{color: currentTheme.blue}">nxshell </span>
-                <span :style="{color: currentTheme.blue}">~</span>
-                <span>]&nbsp;</span>
-                <span :style="{color: currentTheme.red}"># </span>
-                <span>ls -all</span>
-            </div>
-            <div class="theme-color-item">
-                <span>drwxr-xr-x 3 root&nbsp;</span>
-                <span :style="{color: currentTheme.yellow}">Documents</span>
-            </div>
-            <div class="theme-color-item">
-                <span>drwxr-xr-x 3 root&nbsp;</span>
-                <span :style="{color: currentTheme.black,background: currentTheme.green}">Downloads</span>
-            </div>
-            <div class="theme-color-item">
-                <span>drwxr-xr-x 3 root&nbsp;</span>
-                <span :style="{color: currentTheme.black,background: currentTheme.brightBlack}">Pictures</span>
-            </div>
-            <div class="theme-color-item">
-                <span>drwxr-xr-x 3 root&nbsp;</span>
-                <span :style="{color: currentTheme.brightBlue}">Music</span>
-            </div>
-            <div class="theme-color-item">
-                <span>[</span>
-                <span :style="{color: currentTheme.green}">root</span>
-                <span :style="{color: currentTheme.cyan}">@</span>
-                <span :style="{color: currentTheme.blue}">nxshell </span>
-                <span :style="{color: currentTheme.blue}">~</span>
-                <span>]&nbsp;</span>
-                <span :style="{color: currentTheme.red}"># </span>
-                <span :class="{'cursor-blink': cursorBlinkRef}">
+	<div class="n-terminal-preview" v-bind="$attrs">
+		<div class="color-dot-wrapper">
+			<span class="color-name">{{ props.themeName }}</span>
+			<div class="color-dot">
+				<span
+					class="color-dot-item"
+					v-for="(item, index) in colorList"
+					:key="index"
+					:style="{ background: item }"
+				></span>
+			</div>
+		</div>
+		<div class="theme-color" :style="themeStyle">
+			<div class="theme-color-item">
+				<span>[</span>
+				<span :style="{ color: currentTheme.green }">root</span>
+				<span :style="{ color: currentTheme.cyan }">@</span>
+				<span :style="{ color: currentTheme.blue }">nxshell </span>
+				<span :style="{ color: currentTheme.blue }">~</span>
+				<span>]&nbsp;</span>
+				<span :style="{ color: currentTheme.red }"># </span>
+				<span>ls -all</span>
+			</div>
+			<div class="theme-color-item">
+				<span>drwxr-xr-x 3 root&nbsp;</span>
+				<span :style="{ color: currentTheme.yellow }">Documents</span>
+			</div>
+			<div class="theme-color-item">
+				<span>drwxr-xr-x 3 root&nbsp;</span>
+				<span :style="{ color: currentTheme.black, background: currentTheme.green }">Downloads</span>
+			</div>
+			<div class="theme-color-item">
+				<span>drwxr-xr-x 3 root&nbsp;</span>
+				<span :style="{ color: currentTheme.black, background: currentTheme.brightBlack }">Pictures</span>
+			</div>
+			<div class="theme-color-item">
+				<span>drwxr-xr-x 3 root&nbsp;</span>
+				<span :style="{ color: currentTheme.brightBlue }">Music</span>
+			</div>
+			<div class="theme-color-item">
+				<span>drwxr-xr-x 3 root&nbsp;</span>
+				<span :style="{ color: currentTheme.green }">NxShell</span>
+			</div>
+			<div class="theme-color-item">
+				<span>drwxr-xr-x 3 root&nbsp;</span>
+				<span
+					class="n-link"
+					:style="{
+						color: currentTheme.brightBlue,
+						'--data-before': currentTheme.cyan,
+						'--data-after': currentTheme.red
+					}"
+					>-></span
+				>
+			</div>
+			<div class="theme-color-item">
+				<span>[</span>
+				<span :style="{ color: currentTheme.green }">root</span>
+				<span :style="{ color: currentTheme.cyan }">@</span>
+				<span :style="{ color: currentTheme.blue }">nxshell </span>
+				<span :style="{ color: currentTheme.blue }">~</span>
+				<span>]&nbsp;</span>
+				<span :style="{ color: currentTheme.red }"># </span>
+				<span :class="{ 'cursor-blink': cursorBlinkRef }">
 					{{ cursorStyleRef }}
 				</span>
-            </div>
-        </div>
-    </div>
+			</div>
+		</div>
+	</div>
 </template>
 
 <script setup>
-import xtermTheme from "xterm-theme";
-import { onMounted, ref, watchEffect } from "vue";
+import xtermTheme from 'xterm-theme'
+import { onMounted, ref, watchEffect } from 'vue'
 
-const scrollbar = ref()
 const props = defineProps({
-    themeName: {
-        type: String,
-        default: ''
-    },
-    options: {
-        type: Object,
-        default: () => {
-            return {
-                fontFamily: '',
-                fontSize: 12,
-                fontWeight: 400,
-                lineHeight: 1.2,
-                letterSpacing: 1,
-                cursorStyle: 'block',
-                cursorBlink: true
-            }
-        }
-    }
+	themeName: {
+		type: String,
+		default: ''
+	},
+	options: {
+		type: Object,
+		default: () => {
+			return {
+				fontFamily: '',
+				fontSize: 12,
+				fontWeight: 400,
+				lineHeight: 1.2,
+				letterSpacing: 1,
+				cursorStyle: 'block',
+				cursorBlink: true
+			}
+		}
+	}
 })
 const ignoreProps = ['background', 'cursor', 'cursorAccent', 'foreground', 'selection']
 const colorList = ref([])
 const currentTheme = ref({})
-const CURSOR_STYLE = { block: "█", bar: "|", underline: "▁" }
-const cursorStyleRef = ref("█")
+const CURSOR_STYLE = { block: '█', bar: '|', underline: '▁' }
+const cursorStyleRef = ref('█')
 const themeStyle = ref({})
 const cursorBlinkRef = ref()
 
 const initThemeStyle = () => {
-    const { foreground, background } = currentTheme.value
-    const { fontFamily, fontWeight, fontSize, lineHeight, letterSpacing, cursorStyle, cursorBlink } = props.options
-    themeStyle.value = {
-        color: foreground,
-        background: background,
-        fontFamily: fontFamily,
-        fontWeight: fontWeight,
-        fontSize: fontSize + 'px',
-        lineHeight: lineHeight,
-        letterSpacing: letterSpacing + 'px'
-    }
-    cursorBlinkRef.value = cursorBlink
-    cursorStyleRef.value = CURSOR_STYLE[cursorStyle ?? 'bar']
+	const { foreground, background } = currentTheme.value
+	const { fontFamily, fontWeight, fontSize, lineHeight, letterSpacing, cursorStyle, cursorBlink } = props.options
+	themeStyle.value = {
+		color: foreground,
+		background: background,
+		fontFamily: fontFamily,
+		fontWeight: fontWeight,
+		fontSize: fontSize + 'px',
+		lineHeight: lineHeight,
+		letterSpacing: letterSpacing + 'px'
+	}
+	cursorBlinkRef.value = cursorBlink
+	cursorStyleRef.value = CURSOR_STYLE[cursorStyle ?? 'bar']
 }
 
 function initThemePreview() {
-    currentTheme.value = xtermTheme[props.themeName ?? Object.keys(xtermTheme)[0]]
-    colorList.value = []
-    for (const key in currentTheme.value) {
-        if (!ignoreProps.includes(key)) {
-            colorList.value.push(currentTheme.value[key])
-        }
-    }
-    initThemeStyle()
+	currentTheme.value = xtermTheme[props.themeName ?? Object.keys(xtermTheme)[0]]
+	colorList.value = []
+	for (const key in currentTheme.value) {
+		if (!ignoreProps.includes(key)) {
+			colorList.value.push(currentTheme.value[key])
+		}
+	}
+	initThemeStyle()
 }
 
 onMounted(() => {
-    initThemePreview()
+	initThemePreview()
 })
 watchEffect(() => initThemeStyle())
 </script>
 
 <style lang="scss" scoped>
 .n-terminal-preview {
-  width: 100%;
-  height: 100%;
-  display: flex;
-  flex-direction: column;
-  row-gap: 5px;
-  box-sizing: border-box;
+	width: 100%;
+	height: 100%;
+	display: flex;
+	flex-direction: column;
+	row-gap: 5px;
+	box-sizing: border-box;
 
-  &:hover {
-    cursor: pointer;
-  }
+	&:hover {
+		cursor: pointer;
+	}
 
-  .color-dot-wrapper {
-    display: flex;
-    justify-content: space-between;
-    align-items: center;
-    column-gap: 5px;
-    padding: 10px 0;
-    width: 100%;
-    box-sizing: border-box;
+	.color-dot-wrapper {
+		display: flex;
+		justify-content: space-between;
+		align-items: center;
+		column-gap: 5px;
+		padding: 10px 0;
+		width: 100%;
+		box-sizing: border-box;
 
-    &::before {
-      content: '';
-      width: 10px;
-      height: 10px;
-      border-radius: 50%;
-      background-color: #ccc;
-    }
+		&::before {
+			content: '';
+			width: 10px;
+			height: 10px;
+			border-radius: 50%;
+			background-color: #ccc;
+		}
 
-    .color-name {
-      flex: 1;
-      color: var(--n-text-color-base);
-      font-size: 13px;
-      font-weight: bold;
-    }
+		.color-name {
+			flex: 1;
+			color: var(--n-text-color-base);
+			font-size: 13px;
+			font-weight: bold;
+		}
 
-    .color-dot {
-      display: grid;
-      grid-template-columns: repeat(8, 1fr);
-      grid-gap: 2px;
+		.color-dot {
+			display: grid;
+			grid-template-columns: repeat(8, 1fr);
+			grid-gap: 2px;
 
-      .color-dot-item {
-        width: 8px;
-        height: 8px;
-      }
-    }
+			.color-dot-item {
+				width: 8px;
+				height: 8px;
+			}
+		}
+	}
 
-  }
+	.theme-color {
+		display: flex;
+		flex-direction: column;
+		row-gap: 2px;
+		width: 100%;
+		height: 100%;
+		padding: 5px 5px 10px;
+		box-sizing: border-box;
+		backdrop-filter: blur(5px);
+		white-space: nowrap;
+		overflow: hidden;
 
-  .theme-color {
-    display: flex;
-    flex-direction: column;
-    row-gap: 2px;
-    width: 100%;
-    height: 100%;
-    padding: 5px 5px 10px;
-    box-sizing: border-box;
-    backdrop-filter: blur(5px);
-    white-space: nowrap;
-    overflow: hidden;
+		&-item {
+			width: 100%;
+			height: 100%;
 
-    &-item {
-      width: 100%;
-      height: 100%;
-    }
-  }
+			.n-link {
+				&::before {
+					content: 'sym ';
+					color: var(--data-before);
+				}
+
+				&::after {
+					content: ' link';
+					color: var(--data-after);
+				}
+			}
+		}
+	}
 }
 
 .theme-selected {
-  .color-dot-wrapper {
-    &::before {
-      background-color: #1de9b6;
-    }
-  }
+	.color-dot-wrapper {
+		&::before {
+			background-color: #1de9b6;
+		}
+	}
 }
 
 @keyframes blinkFrame {
-  0% {
-    opacity: 1;
-  }
-  25% {
-    opacity: 0.5;
-  }
-  50% {
-    opacity: 0.25;
-  }
-  100% {
-    opacity: 0;
-  }
+	0% {
+		opacity: 1;
+	}
+	25% {
+		opacity: 0.5;
+	}
+	50% {
+		opacity: 0.25;
+	}
+	100% {
+		opacity: 0;
+	}
 }
 
 .cursor-blink {
-  animation: blinkFrame 1s linear 1000ms infinite;
+	animation: blinkFrame 1s linear 1000ms infinite;
 }
 </style>
--- src/views/session/components/xtermTheme/index.vue
+++ src/views/session/components/xtermTheme/index.vue
@@ -1,169 +1,154 @@
 <template>
-    <div class="nx-theme-preview">
-        <div class="nx-theme-search">
-            <el-autocomplete
-                v-model="searchKey"
-                :fetch-suggestions="querySearch"
-                placeholder="请输入内容"
-                :trigger-on-focus="false"
-                clearable
-                @select="handleSearch"
-                @clear="handleSearch"
-            />
-            <el-button type="primary" @click="gotoSelected">当前</el-button>
-        </div>
-        <div ref="themeListRef" class="nx-theme-list">
-            <div class="content">
-                <theme-preview
-                    v-for="(theme, index) in themeList"
-                    :key="index"
-                    :theme-name="theme"
-                    :theme.prop="theme"
-                    :options="themeOptions"
-                    :class="{ 'theme-selected': selectedTheme === theme }"
-                    @click.native="handleSelect"
-                />
-            </div>
-        </div>
-    </div>
+	<div class="nx-theme-preview">
+		<div class="nx-theme-search">
+			<el-autocomplete
+				v-model="searchKey"
+				:fetch-suggestions="querySearch"
+				:placeholder="selectedTheme"
+				:trigger-on-focus="false"
+				clearable
+				@select="handleSearch"
+				@clear="handleSearch"
+			/>
+			<el-button type="primary" @click="gotoSelected">当前</el-button>
+		</div>
+		<div ref="themeListRef" class="nx-theme-list">
+			<div ref="scrollbarRef" class="content">
+				<theme-preview
+					v-for="(theme, index) in themeList"
+					:key="index"
+					:theme-name="theme"
+					:theme.prop="theme"
+					:options="themeOptions"
+					:class="{ 'theme-selected': selectedTheme === theme }"
+					@click.native="handleSelect"
+				/>
+			</div>
+		</div>
+	</div>
 </template>
 
 <script setup>
+import { onMounted, onUpdated, ref, watch } from 'vue'
 import xtermTheme from 'xterm-theme'
 import ThemePreview from './ThemePreview.vue'
-import BScroll from '@better-scroll/core'
-import MouseWheel from '@better-scroll/mouse-wheel'
-import { onMounted, onUnmounted, onUpdated, ref, watch } from 'vue'
-
-BScroll.use(MouseWheel)
 
 const themeListRef = ref()
 const searchKey = ref('')
 const themeList = ref([])
 const scrollbar = ref()
+const scrollbarRef = ref()
 
 const props = defineProps({
-    value: {
-        type: String,
-        default: ''
-    },
-    themeOptions: {
-        type: Object,
-        default: () => {
-            return {
-                fontFamily: '',
-                fontSize: 12,
-                fontWeight: 400,
-                lineHeight: 1.2,
-                letterSpacing: 1,
-                cursorStyle: 'block',
-                cursorBlink: true
-            }
-        }
-    }
+	value: {
+		type: String,
+		default: ''
+	},
+	themeOptions: {
+		type: Object,
+		default: () => {
+			return {
+				fontFamily: '',
+				fontSize: 12,
+				fontWeight: 400,
+				lineHeight: 1.2,
+				letterSpacing: 1,
+				cursorStyle: 'block',
+				cursorBlink: true
+			}
+		}
+	}
 })
 const selectedTheme = ref(props.value)
 const emits = defineEmits(['update:value'])
-
 const gotoSelected = () => {
-    setTimeout(() => {
-        scrollbar.value?.refresh()
-        // 获取当前选中的元素，之后将滚动到该位置
-        const activeElement = document.getElementsByClassName(' theme-selected')[0]
-        activeElement && scrollbar.value?.scrollToElement(activeElement, 500, true, false)
-    }, 100)
+	const activeElement = Array.from(document.getElementsByClassName('theme-selected')).find(
+		(x) => x.theme === selectedTheme.value
+	)
+	if (activeElement) {
+		const searchHeight = document.getElementsByClassName('nx-theme-search')[0].offsetHeight
+		themeListRef.value.scrollTo({ top: activeElement.offsetTop - searchHeight - 5 , behavior: 'smooth' })
+	}
 }
 
-watch(themeList.value, () => {
-    scrollbar.value?.refresh()
-})
-
-watch(() => props.value, () => {
-    selectedTheme.value = props.value
-    gotoSelected()
-})
+watch(
+	() => props.value,
+	() => {
+		selectedTheme.value = props.value
+		setTimeout(gotoSelected, 100)
+	}
+)
 
 onMounted(() => {
-    themeList.value = Object.keys(xtermTheme)
-    scrollbar.value = new BScroll(themeListRef.value, {
-        scrollY: true,
-        click: true,
-        mouseWheel: true,
-        scrollbar: true,
-        probeType: 3
-    })
-
-    selectedTheme.value = props.value
-    gotoSelected()
+	themeList.value = Object.keys(xtermTheme)
+	selectedTheme.value = props.value
+	setTimeout(gotoSelected, 2100) // wait for the first render to make sure the search field is fully visible before applying the selected color scheme.
 })
 
 onUpdated(() => {
-    scrollbar.value?.refresh()
+	scrollbar.value?.refresh()
 })
 
 const handleSelect = (e) => {
-    const themeName = e.currentTarget.theme
-    selectedTheme.value = themeName
-    emits('update:value', themeName)
+	const themeName = e.currentTarget.theme
+	selectedTheme.value = themeName
+	emits('update:value', themeName)
 }
 
 const querySearch = (queryString, cb) => {
-    const restaurants = Object.keys(xtermTheme).map(x => {
-        return { value: x }
-    });
-    const results = queryString ? restaurants.filter(x => x.value.toLowerCase().includes(searchKey.value.toLowerCase())) : restaurants;
-    // // 调用 callback 返回建议列表的数据
-    cb(results);
+	const restaurants = Object.keys(xtermTheme).map((x) => {
+		return { value: x }
+	})
+	const results = queryString
+		? restaurants.filter((x) => x.value.toLowerCase().includes(searchKey.value.toLowerCase()))
+		: restaurants
+	// // 调用 callback 返回建议列表的数据
+	cb(results)
 }
 const handleSearch = (data) => {
-    const themeData = Object.keys(xtermTheme)
-    themeList.value = data ? themeData.filter((x) => {
-        if (x.includes(data.value)) {
-            return x;
-        }
-    }) : themeData
-    !data && gotoSelected()
+	const themeData = Object.keys(xtermTheme)
+	themeList.value = data
+		? themeData.filter((x) => {
+				if (x.includes(data.value)) {
+					return x
+				}
+		  })
+		: themeData
+	!data && gotoSelected()
 }
-
-const refresh = () => gotoSelected()
-defineExpose({ refresh })
-
-onUnmounted(() => scrollbar.value?.destroy())
 </script>
 
 <style lang="scss" scoped>
 .nx-theme-preview {
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  row-gap: 5px;
-  width: 100%;
-  height: 100%;
+	display: flex;
+	flex-direction: column;
+	align-items: center;
+	row-gap: 5px;
+	width: 100%;
+	height: 100%;
 
-  .nx-theme-search {
-    display: flex;
-    column-gap: 10px;
-    width: 100%;
-    z-index: 999;
-    background-color: var(--n-color-bg-dialog);
-    padding-bottom: 5px;
+	.nx-theme-search {
+		display: flex;
+		column-gap: 10px;
+		width: 100%;
+		z-index: 999;
+		background-color: var(--n-color-bg-dialog);
 
-    ::v-deep .el-autocomplete {
-      width: 100%;
-    }
-  }
+		::v-deep .el-autocomplete {
+			width: 100%;
+		}
+	}
 
-  .nx-theme-list {
-    width: 100%;
-    height: 160px;
-    overflow: hidden;
+	.nx-theme-list {
+		width: 100%;
+		overflow: auto;
+		scroll-snap-type: y mandatory;
+		scroll-snap-align: start;
 
-    .content {
-      display: inline-block;
-      width: 100%;
-    }
-  }
-
+		.content {
+			display: inline-block;
+			width: 100%;
+		}
+	}
 }
-
 </style>
