c1986ed4c4fb0e30630dae21ea4665c82c85b3ea
剧终 <13824303245@163.com>
2023-04-12 11:09:15 +0800

    1. 修复并优化编辑器

--- src/store/modules/nx-tabs.ts
+++ src/store/modules/nx-tabs.ts
@@ -1,5 +1,5 @@
-import { defineStore } from "pinia";
-import { getCurrentInstance, onMounted, onUnmounted, ref } from "vue";
+import { defineStore } from 'pinia'
+import { getCurrentInstance, onMounted, onUnmounted, ref } from 'vue'
 import { subscript } from '@/services/eventbus'
 import router from '@/router'
 import { getFileIcon, getFolderIcon, getSystemIcon } from '@/icons/system-icon'
@@ -7,144 +7,147 @@ import { getFileIcon, getFolderIcon, getSystemIcon } from '@/icons/system-icon'
 import { getProfile, updateProfile } from '@/services/globalSetting'
 
 export interface INxTabProps {
-    id: number | -1
-    title: string
-    icon: string
-    iconType: 'img' | 'svg'
-    sessionType: string
-    session: any
+	id: number | -1
+	title: string
+	icon: string
+	iconType: 'img' | 'svg'
+	sessionType: string
+	session: any
 }
 
 const useNxTabsStore = defineStore('nxTabs', () => {
-    const tabData = ref<INxTabProps[]>([])
-    const currentActive = ref<number>(0)
-    const checkedTabType = ref('')
-    const showTabs = ref(true)
-    const configPanel = ref(true)
-    const noConfirm = ref(false)
-    const editorChange = ref(false)
-    const proxy = getCurrentInstance()?.proxy
-    // @ts-ignore
-    const sessionManager = proxy?.$sessionManager
+	const tabData = ref<INxTabProps[]>([])
+	const currentActive = ref<number>(0)
+	const checkedTabType = ref('')
+	const showTabs = ref(true)
+	const configPanel = ref(true)
+	const noConfirm = ref(false)
+	const editorChange = ref(false)
+	const proxy = getCurrentInstance()?.proxy
+	// @ts-ignore
+	const sessionManager = proxy?.$sessionManager
 
-    const tabIcon: Record<string, string> = { "login": "user", "welcome": "logo", "sftp": "folder-sftp-open" }
+	const tabIcon: Record<string, string> = { login: 'user', welcome: 'logo', sftp: 'folder-sftp-open' }
 
-    /**
-     * 监听并更新Tab数据
-     */
-    function updateTabInstance() {
-        const instances = sessionManager.getSessionIntances()
-        tabData.value = instances.map((x: Record<string, any>) => {
-            const { id, type, name, cfg, ext_name } = x
-            const tabInstance: INxTabProps = {
-                id,
-                sessionType: type,
-                title: name === '' ? cfg.hostAddress : name,
-                session: x,
-                iconType: 'svg',
-                icon: tabIcon[type] || 'auto'
-            }
-            if (type === 'shell' || type === 'sftp' || type === 'editor') {
-                tabInstance.title = name === '' ? cfg.hostAddress : name
-                tabInstance.icon = type === 'shell' ? getSystemIcon(cfg.system ?? 'linux') : type === 'editor' ? getFileIcon(ext_name) : tabInstance.icon
-            }
-            return tabInstance
-        })
-    }
+	/**
+	 * 监听并更新Tab数据
+	 */
+	function updateTabInstance() {
+		const instances = sessionManager.getSessionIntances()
+		tabData.value = instances.map((x: Record<string, any>) => {
+			const { id, type, name, cfg, ext_name } = x
+			const tabInstance: INxTabProps = {
+				id,
+				sessionType: type,
+				title: name === '' ? cfg.hostAddress : name,
+				session: x,
+				iconType: 'svg',
+				icon: tabIcon[type] || 'auto'
+			}
+			if (type === 'shell' || type === 'sftp' || type === 'editor') {
+				tabInstance.title = name === '' ? cfg.hostAddress : name
+				tabInstance.icon =
+					type === 'shell'
+						? getSystemIcon(cfg.system ?? 'linux')
+						: type === 'editor'
+						? getFileIcon(ext_name)
+						: tabInstance.icon
+			}
+			return tabInstance
+		})
+	}
 
-    /**
-     * 更新当前激活的Tab签编号
-     *
-     * @param tabIndex 当前tab编号
-     */
-    function updateActiveTabIndex(tabIndex: number) {
-        let index = tabIndex
-        if (currentActive.value === tabIndex) {
-            index = currentActive.value - 1
-            index = currentActive.value > 0 ? currentActive.value : 0
-        }
-        currentActive.value = index
-    }
+	/**
+	 * 更新当前激活的Tab签编号
+	 *
+	 * @param tabIndex 当前tab编号
+	 */
+	function updateActiveTabIndex(tabIndex: number) {
+		let index = tabIndex
+		if (currentActive.value === tabIndex) {
+			index = currentActive.value - 1
+			index = currentActive.value > 0 ? currentActive.value : 0
+		}
+		currentActive.value = index
+	}
 
-    async function activateSessionByInstance(sessionInstance: any) {
-        if (!sessionInstance) {
-            return
-        }
-        const { id, path } = sessionInstance
-        // @ts-ignore
-        if (path === proxy.$route.path) {
-            return
-        }
-        await router.push({ path: sessionInstance.router.path })
-        const activeSessionIndex = tabData.value.findIndex((inst) => inst.id === id)
-        updateActiveTabIndex(activeSessionIndex)
-        sessionInstance.active()
-    }
+	async function activateSessionByInstance(sessionInstance: any) {
+		if (!sessionInstance) {
+			return
+		}
+		const { id, path } = sessionInstance
+		// @ts-ignore
+		if (path === proxy.$route.path) {
+			return
+		}
+		await router.push({ path: sessionInstance.router.path })
+		const activeSessionIndex = tabData.value.findIndex((inst) => inst.id === id)
+		updateActiveTabIndex(activeSessionIndex)
+		sessionInstance.active()
+	}
 
-    /**
-     * 根据选中的菜单高亮匹配的tab
-     *
-     * @param index 当前选中的Tab编号
-     */
-    async function activateSession(index: number) {
-        const instance = tabData.value[index]
-        if (!instance) {
-            return
-        }
-        await activateSessionByInstance(instance.session)
-    }
+	/**
+	 * 根据选中的菜单高亮匹配的tab
+	 *
+	 * @param index 当前选中的Tab编号
+	 */
+	async function activateSession(index: number) {
+		const instance = tabData.value[index]
+		if (!instance) {
+			return
+		}
+		await activateSessionByInstance(instance.session)
+	}
 
-    /**
-     * 更新关闭会话交互
-     *
-     * @param value true | false
-     */
-    function updateNoConfirm(value: boolean) {
-        const defaultSettings = getProfile("xterm")
-        updateProfile("xterm", { ...defaultSettings, noCloseConfirm: value }).then(() => noConfirm.value = value)
-    }
+	/**
+	 * 更新关闭会话交互
+	 *
+	 * @param value true | false
+	 */
+	function updateNoConfirm(value: boolean) {
+		const defaultSettings = getProfile('xterm')
+		updateProfile('xterm', { ...defaultSettings, noCloseConfirm: value }).then(() => (noConfirm.value = value))
+	}
 
-    function updateEditChange(status: boolean) {
-        editorChange.value = status
-    }
+	function updateEditChange(status: boolean) {
+		editorChange.value = status
+	}
 
-    onMounted(() => {
-        // 关闭会话是否需要确认
-        noConfirm.value = getProfile("xterm")?.noCloseConfirm as boolean || false
-        subscript('instance-created', async (sessionInstance: any) => {
-            updateTabInstance()
-            await activateSessionByInstance(sessionInstance)
-        })
-        subscript('instance-destroyed', async () => {
-            updateTabInstance()
-            if (tabData.value.length <= 0) {
-                sessionManager.createWelcomeSessionInstance()
-            } else {
-                if (currentActive.value >= tabData.value.length) {
-                    updateActiveTabIndex(tabData.value.length - 1)
-                }
-                await activateSession(currentActive.value)
-            }
-        })
-    })
+	onMounted(() => {
+		// 关闭会话是否需要确认
+		noConfirm.value = (getProfile('xterm')?.noCloseConfirm as boolean) || false
+		subscript('instance-created', async (sessionInstance: any) => {
+			updateTabInstance()
+			await activateSessionByInstance(sessionInstance)
+		})
+		subscript('instance-destroyed', async () => {
+			updateTabInstance()
+			if (tabData.value.length <= 0) {
+				sessionManager.createWelcomeSessionInstance()
+			} else {
+				if (currentActive.value >= tabData.value.length) {
+					updateActiveTabIndex(tabData.value.length - 1)
+				}
+				await activateSession(currentActive.value)
+			}
+		})
+	})
 
-    onUnmounted(() => {
-
-    })
-    return {
-        tabData,
-        showTabs,
-        configPanel,
-        checkedTabType,
-        currentActive,
-        noConfirm,
-        editorChange,
-        updateEditChange,
-        activateSession,
-        activateSessionByInstance,
-        updateNoConfirm,
-        updateActiveTabIndex
-    }
+	onUnmounted(() => {})
+	return {
+		tabData,
+		showTabs,
+		configPanel,
+		checkedTabType,
+		currentActive,
+		noConfirm,
+		editorChange,
+		updateEditChange,
+		activateSession,
+		activateSessionByInstance,
+		updateNoConfirm,
+		updateActiveTabIndex
+	}
 })
 
 export default useNxTabsStore
--- src/views/editor/editorview.vue
+++ src/views/editor/editorview.vue
@@ -1,28 +1,28 @@
 <template>
-    <div class="pt-vnc-view">
-        <div class="n-editor-toolbar">
-            <div class="n-editor-toolbar__nav">
-                <div class="nav-wrapper">
-                    <i class="el-icon-folder-opened nx-edit-file-open" />
-                    <span class="editor_url">{{ currentPath }}</span>
-                </div>
-            </div>
-            <div class="n-editor-toolbar__actions">
-                <el-select v-model="editorTheme" placeholder="请选择主题">
-                    <el-option v-for="(theme, index) in themes" :label="theme.name" :value="index" :key="index" />
-                </el-select>
-                <el-input
-                    v-model="searchKeyWords"
-                    :placeholder="$t('home.editor.search-tab')"
-                    clearable
-                    class="nx-search-input"
-                    suffix-icon="el-icon-search"
-                    @keydown.enter.native="searchDown"
-                />
-            </div>
-        </div>
-        <div ref="editorRef" class="screen" :style="{ 'font-size': fontSize + 'px' }"></div>
-    </div>
+	<div class="pt-vnc-view">
+		<div class="n-editor-toolbar">
+			<div class="n-editor-toolbar__nav">
+				<div class="nav-wrapper">
+					<i class="el-icon-folder-opened nx-edit-file-open" />
+					<span class="editor_url">{{ currentPath }}</span>
+				</div>
+			</div>
+			<div class="n-editor-toolbar__actions">
+				<el-select v-model="editorTheme" placeholder="请选择主题">
+					<el-option v-for="(theme, index) in themes" :label="theme.name" :value="index" :key="index" />
+				</el-select>
+				<el-input
+					v-model="searchKeyWords"
+					:placeholder="$t('home.editor.search-tab')"
+					clearable
+					class="nx-search-input"
+					suffix-icon="el-icon-search"
+					@keydown.enter.native="searchDown"
+				/>
+			</div>
+		</div>
+		<div ref="editorRef" class="screen" :style="{ 'font-size': fontSize + 'px' }"></div>
+	</div>
 </template>
 
 <script setup>
@@ -46,11 +46,11 @@ import { useNxTabsStore, useSettingStore } from '@/store'
 import { getCurrentInstance, onMounted, ref, watchEffect } from 'vue'
 
 const props = defineProps({
-    mode: {
-        type: String,
-        default: () => 'full'
-    },
-    sessionId: undefined
+	mode: {
+		type: String,
+		default: () => 'full'
+	},
+	sessionId: undefined
 })
 const fontSize = ref(16)
 const scale = ref(0)
@@ -60,7 +60,6 @@ const currentPath = ref('')
 const searchKeyWords = ref('')
 const editorTheme = ref(0)
 const nxTabsStore = useNxTabsStore()
-const { editorChange } = storeToRefs(nxTabsStore)
 const { theme } = storeToRefs(useSettingStore())
 const searchCursor = ref()
 const themeConfig = ref(new Compartment())
@@ -71,173 +70,198 @@ const proxy = getCurrentInstance()?.proxy
 const sessionInstance = proxy.$sessionManager.getSessionInstanceById(props.sessionId)
 
 function getSupportLangMode(type) {
-    switch (type) {
-        case '.js':
-            return javascript()
-        case '.css':
-            return css()
-        case '.cc':
-            return cpp()
-        case '.html':
-            return html()
-        case '.java':
-            return java()
-        case '.json':
-            return json()
-        case '.md':
-            return markdown()
-        case '.php':
-            return php()
-        case '.py':
-            return python()
-        case '.xml':
-            return xml()
-        default:
-            return javascript()
-    }
+	switch (type) {
+		case '.js':
+			return javascript()
+		case '.css':
+			return css()
+		case '.cc':
+			return cpp()
+		case '.html':
+			return html()
+		case '.java':
+			return java()
+		case '.json':
+			return json()
+		case '.md':
+			return markdown()
+		case '.php':
+			return php()
+		case '.py':
+			return python()
+		case '.xml':
+			return xml()
+		default:
+			return javascript()
+	}
 }
 
-const zoom = () => fontSize.value = Math.pow(1.1, scale.value) * 16
-const zoomIn = () => scale.value -= 1 && zoom()
-const zoomOut = () => scale.value += 1 && zoom()
-const zoomOver = () => scale.value = 0 && zoom()
+const zoom = () => (fontSize.value = Math.pow(1.1, scale.value) * 16)
 const searchDown = () => {
-    if (!searchCursor.value) {
-        return
-    }
-    let curr = searchCursor.value.next()
-    if (curr.done) {
-        return
-    }
-    editorInstance.value.dispatch({
-        selection: EditorSelection.create([new SelectionRange(curr.value.from, curr.value.to)]),
-        scrollIntoView: true
-    })
+	if (!searchCursor.value) {
+		return
+	}
+	let curr = searchCursor.value.next()
+	if (curr.done) {
+		return
+	}
+	editorInstance.value.dispatch({
+		selection: EditorSelection.create([new SelectionRange(curr.value.from, curr.value.to)]),
+		scrollIntoView: true
+	})
 }
 watchEffect(() => {
-    currentPath.value = sessionInstance?.remote_path ?? ''
-    editorInstance.value?.dispatch({
-        effects: themeConfig.value.reconfigure([themes[editorTheme.value]])
-    })
+	currentPath.value = sessionInstance?.remote_path ?? ''
+	editorInstance.value?.dispatch({
+		effects: themeConfig.value.reconfigure([themes[editorTheme.value]])
+	})
+	// 监听数据是否有变化
+	nxTabsStore.updateEditChange(editorInstance.value?.state.doc.toString() !== oldValue.value)
 })
+
+const save = async () => {
+	editorValue.value = editorInstance.value?.state.doc.toString()
+	if (editorValue.value !== oldValue.value) {
+		await sessionInstance.writeFileContent(editorValue.value)
+		await sessionInstance.saveToRemote()
+		oldValue.value = editorValue.value
+		proxy.$notify({
+			title: '编辑器保存',
+			message: '修改内容已保存',
+			type: 'success'
+		})
+	}
+}
 
 onMounted(async () => {
+	sessionInstance.on('close', () => {
+		try {
+			proxy.$destroy()
+			proxy.$el.remove()
+		} catch (e) {
+			console.log('sftp editor instance remove error', e)
+		}
+	})
+	await sessionInstance.init()
+	const fileValue = await sessionInstance.readFileContent()
+	editorValue.value = new TextDecoder().decode(fileValue)
 
-    sessionInstance.on('close', () => {
-        try {
-            proxy.$destroy()
-            proxy.$el.remove()
-        } catch (e) {
-            console.log('sftp editor instance remove error', e)
-        }
-    })
-    await sessionInstance.init()
-    const editorValue = await sessionInstance.readFileContent()
-    editorValue.value = new TextDecoder().decode(editorValue)
-
-    sessionInstance.registerCloseCallback(() => {
-        // this.save()
-    })
-    const ctrl_s_key = [
-        {
-            key: 'Ctrl-s',
-            // run: this.save
-        },
-        {
-            key: 'Alt--',
-            run: zoomIn
-        },
-        {
-            key: 'Alt-=',
-            run: zoomOut
-        },
-        {
-            key: 'Alt-0',
-            run: zoomOver
-        }
-    ]
-    const lineWrapping = new Compartment()
-    const extensions = [
-        keymap.of([...defaultKeymap, ...historyKeymap, ...ctrl_s_key, indentWithTab]),
-        lineNumbers(),
-        history(),
-        highlightSelectionMatches(),
-        lineWrapping.of(EditorView.lineWrapping),
-        getSupportLangMode(sessionInstance.ext_name),
-        themeConfig.value.of([themes[editorTheme.value]])
-    ]
-    editorInstance.value = new EditorView({
-        doc: editorValue.value,
-        extensions: extensions,
-        parent: editorRef.value
-    })
-    searchCursor.value = new SearchCursor(editorInstance.value.state.doc, searchKeyWords.value)
+	// 注册关闭事件
+	sessionInstance.registerCloseCallback(save)
+	const ctrl_s_key = [
+		{
+			key: 'Ctrl-s',
+			run: () => save()
+		},
+		{
+			key: 'Alt--',
+			run: () => {
+				if (scale.value > 0) {
+					scale.value -= 1
+					zoom()
+				}
+			}
+		},
+		{
+			key: 'Alt-=',
+			run: () => {
+				scale.value += 1
+				zoom()
+			}
+		},
+		{
+			key: 'Alt-0',
+			run: () => {
+				scale.value = 0
+				zoom()
+			}
+		}
+	]
+	const lineWrapping = new Compartment()
+	// 根据App主题初始化编辑器主题
+	editorTheme.value = { dark: 1, light: 0, pink: 6 }[theme.value]
+	const extensions = [
+		keymap.of([...defaultKeymap, ...historyKeymap, ...ctrl_s_key, indentWithTab]),
+		lineNumbers(),
+		history(),
+		highlightSelectionMatches(),
+		lineWrapping.of(EditorView.lineWrapping),
+		getSupportLangMode(sessionInstance.ext_name),
+		themeConfig.value.of([themes[editorTheme.value]])
+	]
+	editorInstance.value = new EditorView({
+		doc: editorValue.value,
+		extensions: extensions,
+		parent: editorRef.value,
+		tabSize: 4
+	})
+	oldValue.value = editorInstance.value.state.doc.toString()
+	searchCursor.value = new SearchCursor(editorInstance.value.state.doc, searchKeyWords.value)
 })
-
 </script>
 
 <style lang="scss" scoped>
 .pt-vnc-view {
-  height: 100%;
-  width: 100%;
-  background-color: var(--n-bg-color-base);
+	height: 100%;
+	width: 100%;
+	background-color: var(--n-bg-color-base);
 
-  .n-editor-toolbar {
-    display: flex;
-    justify-content: flex-start;
-    align-items: center;
-    height: 40px;
-    padding: 0 5px;
+	.n-editor-toolbar {
+		display: flex;
+		justify-content: flex-start;
+		align-items: center;
+		height: 40px;
+		padding: 0 5px;
 
-    &__nav {
-      flex: 1 0 0;
+		&__nav {
+			flex: 1 0 0;
 
-      .nav-wrapper {
-        display: flex;
-        justify-content: flex-start;
-        align-items: center;
+			.nav-wrapper {
+				display: flex;
+				justify-content: flex-start;
+				align-items: center;
 
-        & > *:not(:last-child) {
-          margin-right: 5px;
-        }
+				& > *:not(:last-child) {
+					margin-right: 5px;
+				}
 
-        .nx-edit-file-open {
-          font-size: 18px;
-          color: var(--n-text-color-base);
-          transition: color 0.2s;
-        }
+				.nx-edit-file-open {
+					font-size: 18px;
+					color: var(--n-text-color-base);
+					transition: color 0.2s;
+				}
 
-        .editor_url {
-          color: var(--n-text-color-base);
-          text-overflow: ellipsis;
-          white-space: nowrap;
-          overflow: hidden;
-          max-width: calc(100% - 20px);
-        }
-      }
-    }
+				.editor_url {
+					color: var(--n-text-color-base);
+					text-overflow: ellipsis;
+					white-space: nowrap;
+					overflow: hidden;
+					max-width: calc(100% - 20px);
+				}
+			}
+		}
 
-    &__actions {
-      flex: 1 0 0;
-      display: flex;
-      justify-content: flex-end;
-      align-items: center;
+		&__actions {
+			flex: 1 0 0;
+			display: flex;
+			justify-content: flex-end;
+			align-items: center;
 
-      & > *:not(:first-child) {
-        margin-left: 5px;
-      }
-    }
-  }
+			& > *:not(:first-child) {
+				margin-left: 5px;
+			}
+		}
+	}
 
-  .screen {
-    width: 100%;
-    height: calc(100% - 40px);
-    overflow: auto;
+	.screen {
+		width: 100%;
+		height: calc(100% - 40px);
+		overflow: auto;
 
-    .cm-editor {
-      height: 100%;
-      overflow: auto;
-    }
-  }
+		.cm-editor {
+			height: 100%;
+			overflow: auto;
+		}
+	}
 }
 </style>
